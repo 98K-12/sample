23e16da25008b2fb07dee896fd6161f686a5a940
98K-12 <12kyoshino@gmail.com>
2025-09-29 21:26:24 +0900

    fix rotate

--- sample.go
+++ sample.go
@@ -7,14 +7,17 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-var flg_x, flg_y, flg_j int8
-var vy float64 = 0.0
-var y float64 = 500.0
-var x float64 = 540.0
-var t float64 = 0.0
-var d float64 = 0.0
+var flg_click, flg_move int8
+var Acc float64 = 0.0               //加速度
+var x float64 = 540.0               //初期X座標
+var y float64 = 500.0               //初期Y座標
+var x_1st, y_1st float64 = 0.0, 0.0 //マウスが初めにクリックされたX,Y座標
+var Rad float64 = 0.0               //ラジアン
+var dx, dy float64 = 0.0, 0.0       //sin, cos値
+var Speed float64 = 3.0
 
 type game struct {
 	img *ebiten.Image
@@ -42,79 +45,53 @@ func (g *game) Draw(screen *ebiten.Image) {
 	// 画像のOption定義
 	op := &ebiten.DrawImageOptions{}
 
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		// ジャンプ用フラグ管理
-		flg_y = 1
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		Cur1stx, Cur1sty := ebiten.CursorPosition()
+		x_1st = float64(Cur1stx)
+		y_1st = float64(Cur1sty)
+	}
 
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		flg_click = 1
 		// 画像の回転
+		// クリックしている間画像の中心点とクリック位置の2点を結ぶ直線方向に回転
 		bounds := g.img.Bounds()
 		op.GeoM.Translate(-float64(bounds.Dx())/2, -float64(bounds.Dy())/2)
-		dx, dy := ebiten.CursorPosition()
-		t = math.Atan2((y-float64(dy)), (x-float64(dx))) + math.Pi*2/4
-		op.GeoM.Rotate(t)
+		Curx, Cury := ebiten.CursorPosition()
+		Rad = math.Atan2((y_1st - float64(Cury)), (x_1st - float64(Curx)))
+		op.GeoM.Rotate(Rad)
 
-		// 画像との距離
-		d = math.Sqrt((x-float64(dx))*(x-float64(dx)) + (y-float64(dy))*(y-float64(dy)))
+		// 画像とクリック位置の2点間の距離
+		//d = math.Sqrt((x-float64(dx))*(x-float64(dx)) + (y-float64(dy))*(y-float64(dy)))
 
 		// 画像描画
 		op.GeoM.Translate(x, y)
 		screen.DrawImage(g.img, op)
 	} else {
-		if flg_y == 1 {
-			flg_j = 1
+		if flg_click == 1 {
+			flg_move = 1
+			flg_click = 0
 		}
-		flg_y = 0
 		bounds := g.img.Bounds()
 		op.GeoM.Translate(-float64(bounds.Dx())/2, -float64(bounds.Dy())/2)
-		op.GeoM.Rotate(t)
+
+		if flg_move == 1 {
+			dx = math.Cos(Rad)
+			dy = math.Sin(Rad)
+
+			x = x + dx*Speed
+			y = y + dy*Speed
+		}
+		op.GeoM.Rotate(Rad)
 		op.GeoM.Translate(x, y)
 		screen.DrawImage(g.img, op)
 	}
 
-	if flg_j == 1 {
-		if d < 100 {
-			vy = -5 // 小ジャンプ
-		} else if 100 < d && d < 200 {
-			vy = -15 // 中ジャンプ
-		} else {
-			vy = -30 // 大ジャンプ
-		}
-	}
-	vy += 0.5 // 速度に加速度を足す（重力）
-	y += vy   // 位置に速度を足す
+	//vy += 1.0 // 速度に加速度を足す（重力）
+	//y += vy   // 位置に速度を足す
 
-	if 500 < y {
-		y = 500
-		flg_j = 0
-	}
-	if y < 0 {
-		y = 0
-		flg_j = 0
-	}
-	/*
-		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-			flg_y = 1
-			flg_x = 1
-		}
-		if flg_y == 1 {
-			vy = -10 // ジャンプ
-		}
-		if flg_x == 1 {
-			x += 2
-		}
-		vy += 0.5 // 速度に加速度を足す（重力）
-		y += vy   // 位置に速度を足す
-
-		if y < 180 {
-			flg_y = 0
-		}
-
-		if 300 < y {
-			y = 300
-			flg_x = 0
-		}
-	*/
-	s := fmt.Sprintln(t+math.Pi*2/4, x, y, d)
+	var deg = Rad * 180 / math.Pi
+	s := fmt.Sprintln(Rad, x, y, dx, dy, deg)
 	ebitenutil.DebugPrint(screen, s)
 }
 
